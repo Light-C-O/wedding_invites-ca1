116fc1092fcbbb4d1480e607aff5a5e0dba04841
Light Osuji (Student) <N00242379@iadt.ie>
2025-10-07 14:38:10 +0100

    finished the show all venues, maske sure everything works

--- resources/views/components/venue-details.blade.php
+++ resources/views/components/venue-details.blade.php
@@ -1,10 +1,16 @@
 @props(['title', 'location', 'capacity', 'price', 'image', 'description'])
 
+<!-- Venue Detail -->
 <div  iv class="border rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition duration-300  mx-auto"> <!-- Limit the overall container width to make the component more compact -->
+
     <!-- Venue Title -->
     <h1 class="font-bold text-black-600 mb-2" style="font-size: 3rem;">{{ $title }}</h1> <!-- Heading with larger text and color -->
     <!-- end title -->
-    <div class="flex items-center gap-8">
+    
+    <!-- Venue Info -->
+    <div class="flex items-center gap-6">
+        
+        <!-- Image -->
         <div class="flex-1">
             <!-- Venue Price -->
                 <h2 class="text-gray-900 text-sm mb-4 underline decoration-solid" style="font-size: 1.5rem;">Price: €{{ $price }}</h2>
@@ -17,24 +23,29 @@
                 </div>
             <!-- end image -->
         </div>
+
+        <!-- Content -->
         <div class="flex-1">
-            <div>
+
+            <div class="mb-4">
                 <!-- Venue Description -->
                     <h3 class="text-gray-800 font-semibold mb-2" style="font-size: 2rem;">Why here?</h3> <!-- Subheading for description -->
                     <p class="text-gray-700 leading-relaxed">{{ $description }}</p> <!-- Text is spaced out for readability -->
                 <!-- end description -->
             </div>
-            <div class="text-gray-500 text-sm italic mb-4" style="font-size: 1rem;">
+            <!-- Locstion & Price -->
+            <div class="text-gray-500 text-sm italic" style="font-size: 1rem;">
                 <!-- Venue Location -->
                     <!-- Emphasizing location with italics and smaller text -->
-                    <h2>Location: {{ $location }}</h2>
+                    <h2 class="mb-4">Location: {{ $location }}</h2>
                 <!-- end location -->
 
                 <!-- Venue Capacity -->
                     <!-- Emphasizing capacity with italics and smaller text -->
-                    <h2>Capacity: {{ $capacity }} people</h2>
+                    <h2 class="mb-4">Capacity: {{ $capacity }} people</h2>
                 <!-- end capacity -->
             </div>
         </div>
     </div>
+    <!-- end info -->
 </div
